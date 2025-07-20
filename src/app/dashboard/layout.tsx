c64260b6e481aad73e57645a60c32aedc3731c45
Jonasz Mielke <jonasz.mielke@gmail.com>
2025-07-20 23:24:26 +0200

    Added Prettier configuration

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,39 +1,32 @@
-import Link from "next/link";
-import styles from "./layout.module.css";
-import Options from "./options";
+import Link from 'next/link'
+import styles from './layout.module.css'
+import Options from './options'
 // import { Image } from "@mantine/core";
-import Image from "next/image";
-import { logos } from "@/types/logos";
+import Image from 'next/image'
+import { logos } from '@/types/logos'
+import { Flex } from '@mantine/core'
 
 export default function DashboardLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-    return (
-        <div>
-            <header className={styles.dashboardHeader}>
-                <section>
-                    <Link href={'/dashboard'} className={styles.logo}>
-                        <Image
-                            src={logos.black.full}
-                            alt='Boardsy icon'
-                            height={120}
-                            width={120}
-                        />
-                    </Link>
-                </section>
-                <section>
-
-                </section>
-                <section>
-                    <Options />
-                </section>
-
-            </header>
-            <section className={styles.appContent}>
-                {children}
-            </section>
-        </div>
-    );
+  return (
+    <div>
+      <header className={styles.dashboardHeader}>
+        <section>
+          <Flex justify='center' align='center'>
+            <Link href={'/dashboard'} className={styles.logo}>
+              <Image src={logos.black.horizontal} alt='Boardsy icon' width={120} height={75} />
+            </Link>
+          </Flex>
+        </section>
+        <section></section>
+        <section>
+          <Options />
+        </section>
+      </header>
+      <section className={styles.appContent}>{children}</section>
+    </div>
+  )
 }
